fd7602baf05e3553ef68b48ddc9dc37976f62a0e
anuj00101 <anujgupta2398@gmail.com>
2024-09-15 16:06:37 +0530

    first commit

---
+++ src/main/java/AB001/anujbro/App.java
@@ -0,0 +1,13 @@
+package AB001.anujbro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
